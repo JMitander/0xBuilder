4adbda9d538589c7e7ea7a73bf63e6d31b25298f
jmitander <john@0xplorer.io>
2024-12-19 21:29:18 +0100

    refactor: rename logger in configuration for improved clarity

--- python/configuration.py
+++ python/configuration.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("Configuration")
 
 dotenv.load_dotenv()
 

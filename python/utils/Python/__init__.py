9edc5565a09188c6d2c172c9ee310924ee1410bb
jmitander <john@0xplorer.io>
2024-12-19 21:11:31 +0100

    refactor: update __all__ declaration to use type hinting for better clarity

--- python/utils/Python/__init__.py
+++ python/utils/Python/__init__.py
@@ -1,4 +1,4 @@
 from .strategyexecutionerror import StrategyExecutionError
 from .strategyconfiguration import StrategyConfiguration
 
-__all__ = ['StrategyExecutionError', 'StrategyConfiguration']
+__all__: list[str] = ['StrategyExecutionError', 'StrategyConfiguration']

81a4a32ffd5e63899605bc4956a003f17b36870c
jmitander <john@0xplorer.io>
2024-12-27 12:10:28 +0100

    Remove unused Python modules and training data files

--- python/utils/Python/__init__.py
+++
@@ -1,6 +0,0 @@
-# /home/mitander/0xBuilder/utils/Python/__init__.py
-
-from .strategyexecutionerror import StrategyExecutionError
-from .strategyconfiguration import StrategyConfiguration
-
-__all__: list[str] = ['StrategyExecutionError', 'StrategyConfiguration']
--- python/utils/Python/strategyexecutionerror.py
+++
@@ -1,6 +0,0 @@
-# /home/mitander/0xBuilder/utils/Python/strategyexecutionerror.py
-class StrategyExecutionError(Exception):
-    """Custom exception for strategy execution failures."""
-    def __init__(self, message: str = "Strategy execution failed"):
-        self.message: str = message
-        super().__init__(self.message)
